84074f5caf1d3f3e62aff25ad08517c72a0982c5
kanayama.yutaka <kanayama.yutaka@tis.co.jp>
2019-10-11 18:27:40 +0900

    セキュリティホールとなるような実装をあえて行ったテストを追加

--- nablarch-jackson-adaptor/src/test/java/nablarch/integration/jaxrs/jackson/Jackson2BodyConverterTest.java
+++ nablarch-jackson-adaptor/src/test/java/nablarch/integration/jaxrs/jackson/Jackson2BodyConverterTest.java
@@ -1,13 +1,20 @@
 package nablarch.integration.jaxrs.jackson;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 import mockit.Mocked;
 import mockit.Expectations;
+import nablarch.fw.web.HttpErrorResponse;
 import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.InputStreamReader;
+import java.io.Serializable;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 /**
  * {@link Jackson2BodyConverter}のテストクラス。
@@ -49,7 +56,7 @@ public class Jackson2BodyConverterTest extends JacksonBodyConverterTestSupport<J
         expectedException.expect(IllegalArgumentException.class);
         expectedException.expectMessage("failed to write response.");
 
-        final TestBean bean = new TestBean("aaa",123L, true);
+        final TestBean bean = new TestBean("aaa", 123L, true);
         new Expectations() {{
             jaxRsContext.getProducesMediaType();
             result = "application/json;charset=utf-8";
@@ -58,5 +65,143 @@ public class Jackson2BodyConverterTest extends JacksonBodyConverterTestSupport<J
         }};
 
         sut.write(bean, executionContext);
+    }
+
+    /**
+     * {@link Serializable} を継承したクラスを使用してjackson-databindのデシリアライズを確認する。
+     * セキュリティホールになる可能性がある。
+     *
+     * @throws Exception 何らかの例外
+     */
+    @Test
+    public void testRead_polymorphic() throws Exception {
+        new Expectations() {{
+            jaxRsContext.getRequestClass();
+            result = Serializable.class;
+            jaxRsContext.getConsumesMediaType();
+            result = "application/json";
+            minTimes = 0;
+            servletRequest.getCharacterEncoding();
+            result = "utf-8";
+            servletRequest.getReader();
+            result = new BufferedReader(new InputStreamReader(new ByteArrayInputStream("[\"nablarch.integration.jaxrs.jackson.Jackson2BodyConverterTest$ExampleBean\", { \"str\" : \"str\" }]".getBytes("utf-8")), "utf-8"));
+        }};
+        Jackson2BodyConverter target = new Jackson2BodyConverter() {
+            @Override
+            protected void configure(ObjectMapper objectMapper) {
+                // この設定によりJSONでデシリアライズするクラスを指定できるようになる。
+                objectMapper.enableDefaultTyping();
+            }
+        };
+        Serializable bean = (Serializable) target.read(request, executionContext);
+        assertThat(((ExampleBean) bean).getStr(), is("str"));
+    }
+
+    /**
+     * {@link Serializable} を継承したクラスを使用してjackson-databindのデシリアライズできないことを確認する。
+     *
+     * @throws Exception エラーレスポンス
+     */
+    @Test
+    public void testRead_polymorphic_fail() throws Exception {
+        expectedException.expect(HttpErrorResponse.class);
+        new Expectations() {{
+            jaxRsContext.getRequestClass();
+            result = Serializable.class;
+            jaxRsContext.getConsumesMediaType();
+            result = "application/json";
+            minTimes = 0;
+            servletRequest.getCharacterEncoding();
+            result = "utf-8";
+            servletRequest.getReader();
+            result = new BufferedReader(new InputStreamReader(new ByteArrayInputStream("[\"nablarch.integration.jaxrs.jackson.Jackson2BodyConverterTest$ExampleBean\", { \"str\" : \"str\" }]".getBytes("utf-8")), "utf-8"));
+        }};
+        Serializable bean = (Serializable) sut.read(request, executionContext);
+    }
+
+    /**
+     * {@link Serializable} のフィールドにアノテーションを付与することでJSONで指定したクラスをデシリアライズできることを確認する。
+     *
+     * @throws Exception 何らかの例外
+     */
+    @Test
+    public void testRead_polymorphic_nested_class() throws Exception {
+        new Expectations() {{
+            jaxRsContext.getRequestClass();
+            result = ExampleBeanInField.class;
+            jaxRsContext.getConsumesMediaType();
+            result = "application/json";
+            minTimes = 0;
+            servletRequest.getCharacterEncoding();
+            result = "utf-8";
+            servletRequest.getReader();
+            result = new BufferedReader(new InputStreamReader(new ByteArrayInputStream("{ \"field1\" : { \"str\" : \"str\" }, \"field2\" : { \"@class\" : \"nablarch.integration.jaxrs.jackson.Jackson2BodyConverterTest$ExampleBean\", \"str\" : \"str\" } }".getBytes("utf-8")), "utf-8"));
+        }};
+        ExampleBeanInField bean = (ExampleBeanInField) sut.read(request, executionContext);
+        assertThat(((ExampleBean) bean.getField2()).getStr(), is("str"));
+    }
+
+    /**
+     * {@link Serializable} のフィールドにアノテーションを付与していてもJSONで指定したクラスをデシリアライズできないことを確認する。
+     *
+     * @throws Exception エラーレスポンス
+     */
+    @Test
+    public void testRead_polymorphic_nested_class_with_annotation_disabled() throws Exception {
+        expectedException.expect(HttpErrorResponse.class);
+        new Expectations() {{
+            jaxRsContext.getRequestClass();
+            result = ExampleBeanInField.class;
+            jaxRsContext.getConsumesMediaType();
+            result = "application/json";
+            minTimes = 0;
+            servletRequest.getCharacterEncoding();
+            result = "utf-8";
+            servletRequest.getReader();
+            result = new BufferedReader(new InputStreamReader(new ByteArrayInputStream("{ \"field1\" : { \"str\" : \"str\" }, \"field2\" : { \"@class\" : \"nablarch.integration.jaxrs.jackson.Jackson2BodyConverterTest$ExampleBean\", \"str\" : \"str\" } }".getBytes("utf-8")), "utf-8"));
+        }};
+        Jackson2BodyConverter target = new Jackson2BodyConverter() {
+            @Override
+            protected void configure(ObjectMapper objectMapper) {
+                // アノテーションを使用してのデシリアライズを禁止する。
+                objectMapper.disable(MapperFeature.USE_ANNOTATIONS);
+            }
+        };
+        ExampleBeanInField bean = (ExampleBeanInField) target.read(request, executionContext);
+    }
+
+    public static class ExampleBean implements Serializable {
+        private String str;
+
+        public String getStr() {
+            return str;
+        }
+
+        public void setStr(String str) {
+            this.str = str;
+        }
+    }
+
+    public static class ExampleBeanInField {
+        private ExampleBean field1;
+
+        @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY)
+        private Serializable field2;
+
+        public ExampleBean getField1() {
+            return field1;
+        }
+
+        public void setField1(ExampleBean field1) {
+            this.field1 = field1;
+        }
+
+        public Serializable getField2() {
+            return field2;
+        }
+
+        public void setField2(Serializable field2) {
+            this.field2 = field2;
+        }
     }
 }
